fe021cafa57f196849302e76814b53d370aabb3c
mued <almued101@gmail.com>
2022-08-27 00:27:37 +0600

    rotation issue solve

--- app/src/main/java/com/example/watchmovie/View/UI/MainActivity.java
+++ app/src/main/java/com/example/watchmovie/View/UI/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.watchmovie.View.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.watchmovie.R;
 import com.example.watchmovie.Service.Model.Result;
@@ -21,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MovieListViewModel movieListViewModel;
     private TopMovieListAdapter movieListAdapter;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        tv1 = findViewById(R.id.rand);
         recyclerView = findViewById(R.id.recyclerview);
         GridLayoutManager linearLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -40,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
                 recyclerView.setAdapter(movieListAdapter);
 
 
+            }
+        });
+
+        movieListViewModel.getRandomNumber().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tv1.setText(s);
             }
         });
     }
--- app/src/main/java/com/example/watchmovie/ViewModel/MovieListViewModel.java
+++ app/src/main/java/com/example/watchmovie/ViewModel/MovieListViewModel.java
@@ -1,19 +1,25 @@
 package com.example.watchmovie.ViewModel;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 import com.example.watchmovie.Service.Model.Result;
 import com.example.watchmovie.Service.Repository.MovieRepository;
 
 import java.util.List;
+import java.util.Random;
 
 public class MovieListViewModel extends AndroidViewModel {
 
     private MovieRepository MRepo;
+    private String TAG = this.getClass().getSimpleName();
+
 
     public MovieListViewModel(@NonNull Application application) {
         super(application);
@@ -24,5 +30,36 @@ public class MovieListViewModel extends AndroidViewModel {
     public LiveData<List<Result>> getTopRatedMovieLists(){
         return MRepo.getTopRatedMovieList();
     }
+
+
+    private MutableLiveData<String> randomNumber;
+
+    public MutableLiveData<String> getRandomNumber(){
+
+        if(randomNumber == null){
+
+            randomNumber = new MutableLiveData<>();
+            createRandomNumber();
+        }
+
+        return randomNumber;
+    }
+
+    public void createRandomNumber() {
+
+        Random random  = new Random();
+        randomNumber.postValue("My Random Number"+ random.nextInt(100-1));
+
+    }
+
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.i(TAG, "View Model Destroyed");
+    }
+
+
+
 
 }
